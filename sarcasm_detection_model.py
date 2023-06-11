9d6b164f7f8a18609a8f337c5bb8f99154725559
Prateek Sharma <prateek.sharma7@gmail.com>
2023-06-11 13:10:36 -0400

    Final changes to 'sarcasm detection model'

--- sarcasm_detection_model.py
+++ sarcasm_detection_model.py
@@ -1,5 +1,8 @@
 import json
 
+import numpy as np
+
+import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -17,14 +20,79 @@ for item in datastore:
     labels.append(item['is_sarcastic'])
     urls.append(item['article_link'])
 
+# Slicing data into training and testing
+
+training_size = int(round(0.8*len(sentences), 0))
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
 
 # Tokenizing and Padding
 
-tokenizer = Tokenizer(oov_token="<OOV>")
-tokenizer.fit_on_texts(sentences)
+vocab_size = 30000  # decided after checking the word_index post tokenization of all sentences 
+max_length = 40   # decided after tokenization and padding of all sentences without maxlen param
+padding_type = 'post'
+trunc_type = 'post'
+
+# # To figure out 'vocab_size' and 'max_length' variables
+# tokenizer = Tokenizer(oov_token="<OOV>")
+# tokenizer.fit_on_texts(sentences)
+# word_index = tokenizer.word_index
+# print(len(word_index.keys()))
+
+# sequences = tokenizer.texts_to_sequences(sentences)
+# sequences_padded = pad_sequences(sequences, padding=padding_type)
+# print(sequences_padded.shape)
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
+tokenizer.fit_on_texts(training_sentences)
+
 word_index = tokenizer.word_index
 
-sequences = tokenizer.texts_to_sequences(sentences)
-padded = pad_sequences(sequences, padding='post')
-print(padded[0])
-print(padded.shape)
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, 
+                                padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, 
+                                padding=padding_type, truncating=trunc_type)
+
+# Conversion to Array before feeing into the ANN Model
+
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels).reshape([-1, 1])
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels).reshape([-1, 1])
+
+# ANN Modeling
+
+embedding_dim = 20
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length), 
+    tf.keras.layers.GlobalAveragePooling1D(), 
+    tf.keras.layers.Dense(24, activation='relu'), 
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+num_epochs = 30
+
+model.fit(training_padded, training_labels, epochs=num_epochs, 
+          validation_data=(testing_padded, testing_labels), verbose=2)
+
+# Sarcasm detection in New Sentences
+
+sentence = [
+    "granny starting to fear spiders in the garden might be real", 
+    "the weather today is bright and sunny"
+]
+
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences, maxlen=max_length, 
+                       padding=padding_type, truncating=trunc_type)
+
+print(model.predict(padded))
--- tokenizer.py
+++ tokenizer.py
@@ -8,6 +8,17 @@ sentences = [
 ]
 
 tokenizer = Tokenizer(num_words = 100)
+
+"""
+if we do not want the tokenizer to omit words during tokenisation of 
+sentences in the testing data for words that exist only in testing 
+sentences but not in training sentences i.e. out of vocab words, we 
+need to define some token value for out of vocab words using a 
+parameter "oov_token" as shown below
+"""
+
+# tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
+
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index)
